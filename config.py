cefb4b6b843ee4073f8bd827fedb2523482351c8
rihangamerz522 <rihangamerz774@gmail.com>
2026-06-19 15:22:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+PREFIX = "!"
+
+EMBED_COLOR = 0x5865F2
+
+BOT_NAME = "QueenZzz"
+BOT_VERSION = "1.0.0"
